41f4658b25f05823c1c6e8ee0c39f6586b4fb53a
Kavinda <rdks.kavinda@gmail.com>
2024-01-15 19:04:16 +0530

    add controllers

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const bodyParser=require('body-parser');
-const cors=require('cors');
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';// for logging
+import dotenv from 'dotenv';
+import router from './routes/route.js';    
 
-require('dotenv').config();
 
-const app=express();
+const app = express();
 
-// Middleware
-app.use(bodyParser.json());
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
+app.disable('x-powered-by');
 
-PORT=process.env.PORT || 8000;
+const PORT= process.env.PORT || 5000;
 
-app.listen(PORT,(req,res)=>{
+app.get('/', (req, res) => {
+    res.status(201).json("Home Get request");
+});
+
+
+//midleware
+app.use('/api', router);
+
+
+
+
+app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
-
-
